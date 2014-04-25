249b1f3decd3315423fc3ad6cdf5b819775c7740
artie37 <artieknapp@dyn-169-226-221-104.wireless.albany.edu>
2014-04-25 11:22:23 -0400

    Struct definitions

--- structs.h
+++ structs.h
@@ -1,14 +1,11 @@
-//
-//  structs.h
-//  Program_5A
-//
-//  Created by artie37 on 4/24/14.
-//  Copyright (c) 2014 artie37. All rights reserved.
-//
+// structs.h
+// Arthur Knapp
+// AK146316
 
-#ifndef Program_5A_structs_h
-#define Program_5A_structs_h
-
-
-
-#endif
+typedef struct list
+{
+    char *name;
+    int size;
+    
+    struct list *next;
+} LinkedList;
